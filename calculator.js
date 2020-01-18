869614558a9c3362b9e9c60d7a242c60f3666777
Kevin Dorsey <dorsey.kevin@gmail.com>
2020-01-18 11:29:58 -0500

    Add functionality to evaluate user input.

--- calculator.js
+++ calculator.js
@@ -1,7 +1,14 @@
 "use strict";
 
+const display = document.querySelector("#display");
+const result = document.querySelector("#result");
+
+let displayValues = [];
+let resultValue = 0;
+let currentValueString = "";
 let isOperatorActive = false;
 let isDecimalActive = false;
+let isDisplayEvaluated = false;
 
 function add(a, b) {
   return a + b;
@@ -29,57 +36,147 @@ function operate(operator, a, b) {
   }
 }
 
-function inputDigit(display, buttonValue) {
-  if (display.innerText === "0") {
-    display.innerText = buttonValue;
-  } else {
-    display.innerText += buttonValue;
+function findOperatorIndex(array, regularExpression) {
+  for (let index in array) {
+    if (array[index].toString().match(regularExpression)) return +index;
+    // index is apparently a string, while a number is anticipated as the return value
   }
+}
+
+function executeOrderOfOperations(operatorsRegEx) {
+  while (true) {
+    let index = findOperatorIndex(displayValues, operatorsRegEx);
+    if (!index) break;
+
+    let newValue = operate(displayValues[index], displayValues[index - 1], displayValues[index + 1]);
+    displayValues.splice(index - 1, 3, newValue);
+  }
+}
+
+function inputDigit(buttonValue) {
+  if (display.innerText !== "0" && !isDisplayEvaluated) {
+    display.innerText += buttonValue;
+  } else {
+    display.innerText = buttonValue;
+  }
+
+  if (isDisplayEvaluated) {
+    currentValueString = buttonValue;
+    result.innerText = "";
+    displayValues = [];
+    isDisplayEvaluated = false;
+  } else {
+    currentValueString += buttonValue;
+  }
+
   isOperatorActive = false;
 }
 
-function inputOperator(display, buttonValue) {
+function inputOperator(buttonValue) {
   if (isOperatorActive) {
-    display.innerText = display.innerText.slice(0,-1) + buttonValue;
+    display.innerText = display.innerText.slice(0, -1) + buttonValue;
   } else if (display.innerText.slice(-1) === ".") {
-    display.innerText = display.innerText.slice(0,-1) + buttonValue;
+    display.innerText = display.innerText.slice(0, -1) + buttonValue;
+    currentValueString = currentValueString.slice(0, -1);
     isOperatorActive = true;
   } else {
-    display.innerText += buttonValue;
+    if (isDisplayEvaluated) display.innerText = resultValue + buttonValue;
+    else display.innerText += buttonValue;
     isOperatorActive = true;
   }
+
+  if (currentValueString !== "") {
+    if (isDisplayEvaluated) {
+      displayValues.push(buttonValue);
+      isDisplayEvaluated = false;
+    } else  {
+      displayValues.push(+currentValueString, buttonValue);
+    }
+    currentValueString = "";
+  } else if (!displayValues.length) {
+    displayValues.push(0, buttonValue);
+  } else {
+    displayValues.splice(-1, 1, buttonValue);
+  }
+  
   isDecimalActive = false;
 }
 
-function inputDecimal(display, buttonValue) {
+function inputDecimal() {
   if (!isDecimalActive) {
-    if (isOperatorActive) {
-      display.innerText += `0${buttonValue}`;
-      isOperatorActive = false;
+    if (!isOperatorActive && !isDisplayEvaluated) {
+      display.innerText += ".";
+      currentValueString += ".";
     } else {
-      display.innerText += buttonValue;
+      if (isOperatorActive) isOperatorActive = false;
+      if (isDisplayEvaluated) {
+        display.innerText = "0.";
+        currentValueString = "0.";
+        result.innerText = "";
+        displayValues = [];
+        isDisplayEvaluated = false;
+      } else {
+        display.innerText += "0.";
+        currentValueString += "0.";
+      }
     }
+
     isDecimalActive = true;
-    // change this boolean toggle to disabling decimal button until operator button is clicked
   }
 }
 
+function inputEqualSign() {
+  if (isOperatorActive) {
+    display.innerText = display.innerText.slice(0, -1) + "=";
+    displayValues.pop();
+  } else if (display.innerText.slice(-1) === ".") {
+    display.innerText = display.innerText.slice(0, -1) + "=";
+    displayValues.push(+currentValueString);
+  } else {
+    display.innerText += "=";
+    displayValues.push(+currentValueString);
+  }
+
+  currentValueString = "";
+  isDecimalActive = false;
+}
+
+function evaluateDisplay() {
+  executeOrderOfOperations(/[*/]/);
+  executeOrderOfOperations(/[+-]/);
+  resultValue = displayValues[0];
+  result.innerText = resultValue;
+  currentValueString += resultValue;
+  isDisplayEvaluated = true;
+
+  if (resultValue % 1 !== 0) isDecimalActive = true;
+}
+
+function resetCalculator() {
+  display.innerText = "0";
+  result.innerText = "";
+  displayValues = [];
+  resultValue = 0;
+  currentValueString = "";
+  isOperatorActive = false;
+  isDecimalActive = false;
+  isDisplayEvaluated = false;
+}
+
 function processButtonClick() {
-  const display = document.querySelector("#display");
   const buttonValue = this.innerText;
 
   if (buttonValue.match(/[0-9]/)) {
-    inputDigit(display, buttonValue);
-  } else if (buttonValue.match(/[/*+-]/)) {
-    inputOperator(display, buttonValue);
+    inputDigit(buttonValue);
+  } else if (buttonValue.match(/[*/+-]/)) {
+    inputOperator(buttonValue);
   } else if (buttonValue === ".") {
-    inputDecimal(display, buttonValue);
+    inputDecimal();
   } else if (buttonValue === "=") {
-    // evaluate expression in display here
+    inputEqualSign();
+    evaluateDisplay();
   } else if (buttonValue === "C") {
-    display.innerText = "0";
-    isOperatorActive = false;
-    isDecimalActive = false;
+    resetCalculator();
   }
 }
 
